1007ff182c449f04551b3900c8ec327dcc9e98f5
Mayrbek <buvaysar1988@mail.ru>
2023-11-15 14:10:36 +0300

    start

--- RainbowGame/RainbowGame/MainViewController/MainViewController.swift
+++ RainbowGame/RainbowGame/MainViewController/MainViewController.swift
@@ -5,14 +5,19 @@
 //  Created by sidzhe on 12.11.2023.
 //
 
+import SnapKit
 import UIKit
 
 final class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initialize()
+    
         
-        
+    }
+    
+    func initialize() {
         
     }
 }
